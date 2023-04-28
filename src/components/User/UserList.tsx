b3f53a48a2385cf540cc0f0dcc161050f73f0cfd
shofwanh <shofwanhanif22@gmail.com>
2023-04-28 11:20:40 +0800

    Add: Adjust component user list

--- src/components/User/UserList.tsx
+++ src/components/User/UserList.tsx
@@ -5,21 +5,31 @@ const UserList = () => {
   const { query, users, repositories } = useGithub();
   return (
     <>
-      {users.length > 0 && <p className="mb-2">Showing Users for "{query}"</p>}
-      {users.map((user) => (
-        <div key={user.login} className="collapse collapse-arrow mb-2">
-          <input type="checkbox" className="peer" />
-          <div className="collapse-title bg-slate-200 text-black">
-            {user.login}
-          </div>
-          <div className="collapse-content text-black ml-3 mt-2 pr-0">
-            {repositories.length > 0 &&
-              repositories
-                .filter((repo) => repo.owner.login === user.login)
-                .map((repo) => <UserRepo key={repo.name} children={repo} />)}
-          </div>
-        </div>
-      ))}
+      {query ? (
+        <>
+          {users.length > 0 && (
+            <p className="mb-2">Showing Users for "{query}"</p>
+          )}
+          {users.map((user) => (
+            <div key={user.login} className="collapse collapse-arrow mb-2">
+              <input type="checkbox" className="peer" />
+              <div className="collapse-title bg-slate-200 text-black">
+                {user.login}
+              </div>
+              <div className="collapse-content text-black ml-3 mt-2 pr-0">
+                {repositories.length > 0 &&
+                  repositories
+                    .filter((repo) => repo.owner.login === user.login)
+                    .map((repo) => (
+                      <UserRepo key={repo.name} children={repo} />
+                    ))}
+              </div>
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </>
   );
 };
